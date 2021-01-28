258dc91ec1c7a09997c5de56ac8aa1bcdb452235
rozierguillaume <guillaume.rozier73@gmail.com>
2021-01-28 21:12:50 +0100

    uptate

--- scripts/opendata.py
+++ scripts/opendata.py
@@ -2,18 +2,22 @@ import json
 import pandas as pd
 import requests
 
-def download_france_data():
+## FRANCE
+def download_fra_data():
   url = "https://www.data.gouv.fr/fr/datasets/r/efe23314-67c4-45d3-89a2-3faef82fae90"
   data = requests.get(url)
 
   with open('data/input/vacsi-fra.csv', 'wb') as f:
           f.write(data.content)
 
-def import_france_data():
-  df = pd.read_csv('data/input/vacsi-fra.csv', sep=';')
-  return df
+def download_reg_data():
+  url = "https://www.data.gouv.fr/fr/datasets/r/735b0df8-51b4-4dd2-8a2d-8e46d77d60d8"
+  data = requests.get(url)
 
-def csv_to_json(df):
+  with open('data/input/vacsi-reg.csv', 'wb') as f:
+          f.write(data.content)
+
+def csv_to_json_fra(df):
   dict_json = {}
   dict_json["dates"] = df["jour"].tolist()
   dict_json["n_dose1"] = df["n_dose1"].tolist()
@@ -21,6 +25,36 @@ def csv_to_json(df):
   with open("data/output/vacsi-fra.json", "w") as outfile: 
     outfile.write(json.dumps(dict_json))
 
-download_france_data()
-df = import_france_data()
-csv_to_json(df)
+## REGIONS
+def import_fra_data():
+  df = pd.read_csv('data/input/vacsi-fra.csv', sep=';')
+  return df
+
+def import_reg_data():
+  df = pd.read_csv('data/input/vacsi-reg.csv', sep=';')
+  return df
+
+def csv_to_json_reg(df):
+  list_json = []
+
+  for i in range(len(df)):
+    dict_json = {}
+    dict_json["date"] = df.loc[i,"jour"]
+    dict_json["code"] = "REG-" + str(df.loc[i,"reg"])
+    dict_json["n_dose1"] = int(df.loc[i, "n_dose1"])
+    dict_json["n_cum_dose1"] = int(df.loc[i, "n_cum_dose1"])
+
+    list_json += [dict_json]
+
+  with open("data/output/vacsi-reg.json", "w") as outfile: 
+    outfile.write(json.dumps(list_json, indent=4))
+
+
+
+download_fra_data()
+df = import_fra_data()
+csv_to_json_fra(df)
+
+download_reg_data()
+df = import_reg_data()
+csv_to_json_reg(df)
