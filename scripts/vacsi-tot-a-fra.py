52acfc0a11524bb7eb75d7603a7c2e9083f1cfe7
rozierguillaume <guillaume.rozier73@gmail.com>
2021-01-29 19:27:03 +0100

    vacc pop

--- scripts/vacsi-tot-a-fra.py
+++ scripts/vacsi-tot-a-fra.py
@@ -9,17 +9,26 @@ def download_fra_data():
 
   with open('data/input/vacsi-tot-a-fra.csv', 'wb') as f:
           f.write(data.content)
+          
+def prepare_data(df):
+  df_clage_vacsi = pd.read_csv('data/input/clage_spf.csv', sep=';')
+  df = df.merge(df_clage_vacsi, left_on="clage_vacsi", right_on="code_spf").groupby(["jour", "categorie-large"]).sum().reset_index()
+  return df
 
 def import_fra_data():
   df = pd.read_csv('data/input/vacsi-tot-a-fra.csv', sep=';')
-  return df
+  return prepare_data(df)
 
 def csv_to_json_fra(df):
   jour_max = df.jour.max()
   df = df[(df.clage_vacsi != 0) & (df.jour == jour_max)]
-  df = df.sort_values(by="clage_vacsi")
+  df = df.sort_values(by="categorie-large")
 
-  dict_json = {"age": [str(age) + " ans" for age in df.clage_vacsi.tolist()], "n_dose1": df.n_tot_dose1.tolist(), "n_dose2": df.n_tot_dose2.tolist()}
+  dict_json = {"age": [str(age) + " ans" for age in df["categorie-large"].tolist()], 
+              "n_dose1": df.n_tot_dose1.tolist(), 
+              "n_dose2": df.n_tot_dose2.tolist(),
+              "n_dose1_pop": list(df.n_tot_dose1.values/df.population.values*100),
+              "n_dose2_pop": list(df.n_tot_dose2.values/df.population.values*100)}
 
   with open("data/output/vacsi-tot-a-fra_lastday.json", "w") as outfile:
     outfile.write(json.dumps(dict_json))
