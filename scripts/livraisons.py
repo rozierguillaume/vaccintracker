cbb6d82cea66a3632c938cdc220c60df40fdfd08
rozierguillaume <guillaume.rozier73@gmail.com>
2021-04-02 20:23:54 +0200

    fix

--- scripts/livraisons.py
+++ scripts/livraisons.py
@@ -42,12 +42,15 @@ def download_tot_nat():
 
 def import_tot_nat():
   df = pd.read_csv('data/input/flux-tot-nat.csv', sep=';')
+  print(df)
+  df["date_fin_semaine"] = parsedate(df.date_fin_semaine)
+  print(df)
   return df
 
 def csv_to_json_tot_nat(df):
-  df_tous = df.groupby("date_fin_semaine").sum().reset_index()
+  df_tous = df.groupby("date_fin_semaine").sum().reset_index() #.sort_values(by="date_fin_semaine")
 
-  dict_json = {"jour": parsedate(list(df_tous.date_fin_semaine)),
+  dict_json = {"jour": list(df_tous.date_fin_semaine),
               "nb_doses_tot_cumsum": list(df_tous.nb_doses.cumsum())}
 
   df_pfizer = df[df.type_de_vaccin == "Pfizer"].merge(df["date_fin_semaine"], left_on="date_fin_semaine", right_on="date_fin_semaine", how="right").groupby("date_fin_semaine").first().reset_index().sort_values(by="date_fin_semaine")
@@ -100,14 +103,21 @@ def import_data_flux_separes():
 
   df_flux_moderna = pd.read_csv('data/input/flux-moderna-nat.csv', sep=None, engine='python')
   df_flux_astrazeneca = pd.read_csv('data/input/flux-astrazeneca-nat.csv', sep=None, engine='python')
+
+  df_flux_astrazeneca["date_fin_semaine"] = parsedate(df_flux_astrazeneca.date_fin_semaine.tolist())
+
+  #print(df_flux_moderna)
   return df_flux_pfizer, df_flux_moderna, df_flux_astrazeneca
 
 def parsedate(dates):
   newdates = []
   for date in dates:
-    if "/" in date:
+    if (("/" in date) & ("-" not in date)):
       split = date.split("/")
-      newdates += [split[2] + "-" + split[1] + "-" + split[0]]
+      if(len(split)>=3):
+        newdates += [split[2] + "-" + split[1] + "-" + split[0]]
+      else:
+        newdates += [date]
   return newdates
 
 def csv_to_json(df):
@@ -125,7 +135,7 @@ def csv_to_json_flux_separes(df_flux_pfizer, df_flux_moderna, df_flux_astrazenec
   dict_json_moderna = {"jour": list(df_flux_moderna.date_fin_semaine),
               "nb_doses_tot_cumsum": list(df_flux_moderna.nb_doses.cumsum())}
 
-  dict_json_astrazeneca = {"jour": parsedate(list(df_flux_astrazeneca.date_fin_semaine)),
+  dict_json_astrazeneca = {"jour": list(df_flux_astrazeneca.date_fin_semaine),
               "nb_doses_tot_cumsum": list(df_flux_astrazeneca.nb_doses.cumsum())}
 
   dict_json={1: dict_json_pfizer, 2: dict_json_moderna, 3: dict_json_astrazeneca}
